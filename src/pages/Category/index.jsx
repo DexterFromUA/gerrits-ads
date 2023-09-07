14a1e5300ed7ed94b22e7c57bb25bc71de5574f9
Vladislav Shalimov <vshalimov@mindk.com>
2023-09-07 11:06:05 +0300

    added removing

--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -1,170 +1,131 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-import { getSingleCollection, addFileToStorage, updateCollection } from "../../api";
-
-const MOCK = [
-  {
-    title: "first",
-    description: "description",
-    image:
-      "https://cdn2.vectorstock.com/i/1000x1000/48/06/image-preview-icon-picture-placeholder-vector-31284806.jpg",
-  },
-  {
-    title: "second",
-    description: "description",
-    image:
-      "https://cdn2.vectorstock.com/i/1000x1000/48/06/image-preview-icon-picture-placeholder-vector-31284806.jpg",
-  },
-  {
-    title: "third",
-    description: "description",
-    image:
-      "https://cdn2.vectorstock.com/i/1000x1000/48/06/image-preview-icon-picture-placeholder-vector-31284806.jpg",
-  },
-];
+import { AdImage } from '../../components'
+import {
+  getSingleCollection,
+  addFileToStorage,
+  updateCollection,
+  removeAd
+} from "../../api";
 
 const Category = () => {
   const { id } = useParams();
 
-  const inputRef = React.useRef(null)
+  const inputRef = React.useRef(null);
   const [collection, setCollection] = React.useState(null);
-  const [newFile, setNewFile] = React.useState(null)
+  const [newFile, setNewFile] = React.useState(null);
 
   React.useEffect(() => {
-    handleCollection(id)
-  }, [id])
+    handleCollection(id);
+  }, [id]);
 
   const handleCollection = async (colName) => {
-    const data = await getSingleCollection(colName)
+    const data = await getSingleCollection(colName);
 
     if (data) {
-      setCollection({ ...data })
+      setCollection({ ...data });
     }
-  }
+  };
 
   const handleFileAdding = (e) => {
     if (e.target.files) {
       setNewFile(e.target.files[0]);
     }
-  }
+  };
 
   const handleAddFile = async () => {
     if (newFile) {
-      await addFileToStorage(id, newFile, (url) => {
+      await addFileToStorage(id, newFile, (url, fileName) => {
         if (url) {
           updateCollection(id, {
             ...collection,
-            data: collection?.data ? [...collection.data, url] : [url]
-          })
+            data: collection?.data ? [...collection.data, [url, fileName]] : [[url, fileName]],
+          });
 
-          setCollection(prevState => ({ ...prevState, data: prevState.data ? [...prevState.data, url] : [url] }))
+          setCollection((prevState) => ({
+            ...prevState,
+            data: prevState.data ? [...prevState.data, [url, fileName]] : [[url, fileName]],
+          }));
         }
-      })
+      });
 
-      setNewFile(null)
+      setNewFile(null);
     } else {
-      inputRef.current.click()
+      inputRef.current.click();
+    }
+  };
+
+  const handleRemovingAd = async (fileName, index) => {
+    const result = await removeAd(id, fileName)
+
+    if (result) {
+      const newData = collection.data.filter((_, i) => i !== index)
+
+      updateCollection(id, {
+        ...collection,
+        data: [...newData]
+      });
+
+      setCollection(prevState => ({
+        ...prevState,
+        data: [...newData]
+      }))
     }
   }
 
   if (!collection) {
-    return (
-      <div>Loading...</div>
-    )
+    return <div>Loading...</div>;
   }
 
   return (
     <div
-      style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        flex: 1
+      }}
     >
-      <div>
-        <h1>{collection.name}</h1>
-      </div>
+      <div style={{ display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', width: '100%' }}>
+        <div>
+          <h1>{collection.name}</h1>
+          <h3>{collection.description}</h3>
+        </div>
 
-      <div>
-        <h3>{collection.description}</h3>
-      </div>
-
-      {collection && collection.data && <div style={{ flex: 1, marginTop: 100, width: 500 }}>
-        {collection.data.map((el, i) => (
-          <div
-            key={i}
-            // onClick={() => {
-            //   console.log("collection");
-            // }}
+        <div>
+          <input
+            type="file"
+            onChange={handleFileAdding}
+            ref={inputRef}
             style={{
-              display: "flex",
-              border: "1px solid black",
-              flexDirection: "row",
-              alignItems: "center",
-              borderRadius: 15,
-              marginBottom: 10,
-              // cursor: "pointer",
-              justifyContent: "space-between",
+              display: "none",
             }}
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
-              <div
-                style={{
-                  marginRight: 50,
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  padding: 10,
-                }}
-              >
-                <img src={el} width={300} height={300} alt="preview" />
-              </div>
+          />
 
-              {/* <div>
-                <h4>{el.title}</h4>
+          <button onClick={() => handleAddFile()}>
+            {newFile ? `Upload: ${newFile.name}` : "Add file"}
+          </button>
+        </div>
+      </div>
 
-                <h6>{el.description}</h6>
-              </div> */}
-            </div>
-
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-
-                console.log("settings");
-              }}
-              style={{
-                display: "flex",
-                alignSelf: "flex-start",
-                justifySelf: "flex-end",
-                padding: 15,
-                cursor: "pointer",
-              }}
-            >
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/126/126472.png"
-                width={20}
-                height={20}
-                alt="settings"
-              />
-            </div>
-          </div>
-        ))}
-      </div>}
-
-      <input
-        type='file'
-        onChange={handleFileAdding}
-        ref={inputRef}
+      <div
         style={{
-          display: "none",
+          display: "flex",
+          flexDirection: "row",
+          flex: 1,
+          flexWrap: "wrap",
+          marginLeft: 50,
+          marginRight: 50,
+          justifyContent: "center",
         }}
-      />
-
-      <button onClick={() => handleAddFile()}>{newFile ? `Upload: ${newFile.name}` : 'Add file'}</button>
+      >
+        {collection &&
+          collection.data &&
+          collection.data.map((el, i) => (
+            <AdImage key={i} image={el[0]} onRemove={() => handleRemovingAd(el[1], i)} />
+          ))}
+      </div>
     </div>
   );
 };
