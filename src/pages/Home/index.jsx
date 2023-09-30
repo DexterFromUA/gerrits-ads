3fc2e4dfcf1531fa281e5b4e574b0440d8aea0b2
Vladislav Shalimov <vshalimov@mindk.com>
2023-09-30 22:23:47 +0300

    fb service added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-import { addCollection, getCollections, signOutUser } from "../../api";
+import { addCollection, getCollections, signOutUser } from "../../services/fbService";
 import preview from '../../images/preview.jpg'
 
 const Home = () => {
